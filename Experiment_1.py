d8f23fc34f66aa36db05caf13d7fde262f2e1f42
admin <billy.bobxii@gmail.com>
2022-09-02 23:52:18 -0500

    done for the day!

--- Experiment_1.py
+++ Experiment_1.py
@@ -1,3 +1,24 @@
 import tkinter as tk
 
-tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.quit_widget()
+        self.hello_world()
+
+    def quit_widget(self):
+        tk.Button(self, text='Quit', command=self.quit).grid()
+
+    def hello_world(self):
+        tk.Button(self, text='Hello World').grid()
+
+
+app = Application()
+app.master.title('This is the application title') # this doesn't work
+app.mainloop()
+
+# root = Tk()
+# ttk.Button(root, text="Hello World").grid()
+# root.mainloop()
